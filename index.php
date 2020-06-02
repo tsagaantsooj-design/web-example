3bf2b9b33d50b0e22192072427f71627800faa40
tsagaantsooj-design <61138473+tsagaantsooj-design@users.noreply.github.com>
2020-06-02 08:27:52 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-/*
- * @author Shahrukh Khan
- * @website http://www.thesoftwareguy.in
- * @facebook https://www.facebook.com/Thesoftwareguy7
- * @twitter https://twitter.com/thesoftwareguy7
- * @googleplus https://plus.google.com/+thesoftwareguyIn
- */
+
 header("Location:home.php");
 ?>
